6c30f848f408603b8d9a5734d84e70ca2fadbc8d
gr413_suhg <fireboll.h@mail.ru>
2023-02-03 10:19:52 +0700

    реализовал 3 практическое задание

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        nStock: true,
+        onSale: true
     }
 })
