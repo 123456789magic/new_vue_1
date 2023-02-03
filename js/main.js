0b48da29ed35081357fd461986c2b15de89c1157
gr413_suhg <fireboll.h@mail.ru>
2023-02-03 10:37:41 +0700

    реализовал 5 практическое задание

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ let app = new Vue({
         description: 'A pair of warm fuzzy socks',
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: true,
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         nStock: true,
         onSale: true,
@@ -14,13 +13,24 @@ let app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
             }
         ],
+        cart: 0,
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
